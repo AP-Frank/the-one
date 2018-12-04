ae424514eb312f550a205b2c287d36e349f07e97
Alexander Frank <alexander-pascal.frank@tum.de>
2018-12-04 21:46:35 +0100

    add JSON parsing

--- src/core/DTNSim.java
+++ src/core/DTNSim.java
@@ -46,6 +46,8 @@ public class DTNSim {
 		int firstConfIndex = 0;
 		int guiIndex = 0;
 
+		RoomMapper.initialize("../resources/fullGround.json");
+
 		/* set US locale to parse decimals in consistent way */
 		java.util.Locale.setDefault(java.util.Locale.US);
 
---
+++ src/core/Room.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Room {
+    public String Tag;
+    public double PosX;
+    public double PosY;
+}
