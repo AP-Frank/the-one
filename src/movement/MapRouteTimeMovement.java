2949d6f462f6f72cb5129afda4edaf09dcbd2d27
Alexander Frank <alexander-pascal.frank@tum.de>
2018-12-04 17:22:26 +0100

    remove everything related to routes based on route files

--- src/movement/MapRouteTimeMovement.java
+++ src/movement/MapRouteTimeMovement.java
@@ -2,104 +2,47 @@ package movement;
 
 import core.Coord;
 import core.Settings;
-import core.SettingsError;
 import core.SimClock;
 import movement.map.DijkstraPathFinder;
 import movement.map.MapNode;
-import movement.map.MapRoute;
 import movement.map.SimMap;
 
 
 import java.util.List;
 
-public class MapRouteTimeMovement extends MapBasedMovement implements SwitchableMovement{
-
-    /** Per node group setting used for selecting a route file ({@value}) */
-    public static final String ROUTE_FILE_S = "routeFile";
+public class MapRouteTimeMovement extends MapBasedMovement implements SwitchableMovement {
     /**
-     * Per node group setting used for selecting a route's type ({@value}).
-     * Integer value from {@link MapRoute} class.
+     * the Dijkstra shortest path finder
      */
-    public static final String ROUTE_TYPE_S = "routeType";
-
-    /**
-     * Per node group setting for selecting which stop (counting from 0 from
-     * the start of the route) should be the first one. By default, or if a
-     * negative value is given, a random stop is selected.
-     */
-    public static final String ROUTE_FIRST_STOP_S = "routeFirstStop";
-
-    /** the Dijkstra shortest path finder */
     private DijkstraPathFinder pathFinder;
-
-    /** Prototype's reference to all routes read for the group */
-    private List<MapRoute> allRoutes = null;
-    /** next route's index to give by prototype */
-    private Integer nextRouteIndex = null;
-    /** Index of the first stop for a group of nodes (or -1 for random) */
-    private int firstStopIndex = -1;
-
-    /** Route of the movement model's instance */
-    private MapRoute route;
 
     /**
      * Creates a new movement model based on a Settings object's settings.
+     *
      * @param settings The Settings object where the settings are read from
      */
     public MapRouteTimeMovement(Settings settings) {
         super(settings);
-        String fileName = settings.getSetting(ROUTE_FILE_S);
-        int type = settings.getInt(ROUTE_TYPE_S);
-        allRoutes = MapRoute.readRoutes(fileName, type, getMap());
-        nextRouteIndex = 0;
         pathFinder = new DijkstraPathFinder(getOkMapNodeTypes());
-        this.route = this.allRoutes.get(this.nextRouteIndex).replicate();
-        if (this.nextRouteIndex >= this.allRoutes.size()) {
-            this.nextRouteIndex = 0;
-        }
-
-        if (settings.contains(ROUTE_FIRST_STOP_S)) {
-            this.firstStopIndex = settings.getInt(ROUTE_FIRST_STOP_S);
-            if (this.firstStopIndex >= this.route.getNrofStops()) {
-                throw new SettingsError("Too high first stop's index (" +
-                        this.firstStopIndex + ") for route with only " +
-                        this.route.getNrofStops() + " stops");
-            }
-        }
     }
 
     /**
      * Copyconstructor. Gives a route to the new movement model from the
      * list of routes and randomizes the starting position.
+     *
      * @param proto The MapRouteMovement prototype
      */
     protected MapRouteTimeMovement(MapRouteTimeMovement proto) {
         super(proto);
-        this.route = proto.allRoutes.get(proto.nextRouteIndex).replicate();
-        this.firstStopIndex = proto.firstStopIndex;
-
-        if (firstStopIndex < 0) {
-            /* set a random starting position on the route */
-            this.route.setNextIndex(rng.nextInt(route.getNrofStops()-1));
-        } else {
-            /* use the one defined in the config file */
-            this.route.setNextIndex(this.firstStopIndex);
-        }
-
         this.pathFinder = proto.pathFinder;
-
-        proto.nextRouteIndex++; // give routes in order
-        if (proto.nextRouteIndex >= proto.allRoutes.size()) {
-            proto.nextRouteIndex = 0;
-        }
     }
 
     @Override
     public Path getPath() {
         Path p = new Path(generateSpeed());
-        SimMap map = this.getMap() ;
+        SimMap map = this.getMap();
         List<MapNode> allNodes = map.getNodes();
-        Coord sketch = new Coord(600, 100);
+        Coord sketch = new Coord(1290, 796);
         MapNode nextNode = map.getNodeByCoord(sketch);
 
         List<MapNode> nodePath = pathFinder.getShortestPath(lastMapNode, nextNode);
@@ -108,10 +51,11 @@ public class MapRouteTimeMovement extends MapBasedMovement implements Switchable
                 nextNode + ". The simulation map isn't fully connected";
 
         double time = SimClock.getTime();
-        //in this example, x0 has a break during timing interval [1000ms,4000ms] and moves around the three nodes:
+        //in this example, x0 has a break during timing interval [1000ms,4000ms] and moves around
+        // the three nodes:
         double beginBreak = 1000;
         double endBreak = 4000;
-        if(time >= beginBreak && time <= endBreak) {
+        if (time >= beginBreak && time <= endBreak) {
 
             for (MapNode node : nodePath) { // create a Path from the shortest path
                 p.addWaypoint(node.getLocation());
@@ -131,7 +75,7 @@ public class MapRouteTimeMovement extends MapBasedMovement implements Switchable
     @Override
     public Coord getInitialLocation() {
         if (lastMapNode == null) {
-            lastMapNode = route.nextStop();
+            lastMapNode = this.getMap().getNodeByCoord(new Coord(0, 0));
         }
 
         return lastMapNode.getLocation().clone();
@@ -150,14 +94,6 @@ public class MapRouteTimeMovement extends MapBasedMovement implements Switchable
     @Override
     public MapRouteTimeMovement replicate() {
         return new MapRouteTimeMovement(this);
-    }
-
-    /**
-     * Returns the list of stops on the route
-     * @return The list of stops
-     */
-    public List<MapNode> getStops() {
-        return route.getStops();
     }
 
 }
