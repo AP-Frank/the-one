819d8284381d92cde6949802a0686d5a0b70de9e
Sascha Sauermann <sascha.sauermann@tum.de>
2018-12-11 23:43:09 +0100

    More bugfixes

--- src/schedules/Activity.java
+++ src/schedules/Activity.java
@@ -1,22 +1,26 @@
 package schedules;
 
+import java.text.SimpleDateFormat;
 import java.util.Objects;
 
 public class Activity implements Comparable<Activity> {
     public final String location;
     public final int start;
+    public final int end;
     public final int duration;
     public final String name;
+    private SimpleDateFormat df = new SimpleDateFormat("HH:mm");
 
-    public Activity(String location, int start, int duration, String name) {
+    public Activity(String location, int start, int end, String name) {
         this.location = location;
         this.start = start;
-        this.duration = duration;
+        this.end = end;
+        this.duration = end - start;
         this.name = name;
     }
 
     public boolean overlapsTime(int time) {
-        return time >= start && time < start + duration;
+        return time >= start && time < end;
     }
 
     @Override
@@ -41,6 +45,6 @@ public class Activity implements Comparable<Activity> {
 
     @Override
     public String toString() {
-        return location + " - " + name + " ["+ start + "," + duration + "]";
+        return location + " - " + name + " [" + df.format(start * 1000) + "," + df.format(end * 1000) + "]";
     }
 }
